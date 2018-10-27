b2b799058d673b890343e6b3e9d0efeb4dea9cf3
anass ibnou ali <a.ibnouali@insea.ac.ma>
2018-10-27 13:30:36 +0100

    passwordEncoder :(

--- src/main/java/com/comptes/security/SecurityConfig.java
+++ src/main/java/com/comptes/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.comptes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -22,4 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers("/operations", "/consulterCompte").hasRole("USER");
 		http.csrf().disable().authorizeRequests().antMatchers("/enregistrerOperation").hasRole("ADMIN");
 		http.exceptionHandling().accessDeniedPage("/403"); 	}
+
+@Bean
+public static NoOpPasswordEncoder passwordEncoder() {
+return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+}
 }
